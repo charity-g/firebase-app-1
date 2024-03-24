26878b4875a9b1dcaf9e7d136de7ee62712ca51f
Charity <charity.grey1@gmail.com>
2024-03-24 16:30:43 -0700

    imported firebase database module

--- public/index.js
+++ public/index.js
@@ -1,16 +1,22 @@
 import {initializeApp} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import {getDatabase, ref, push} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
 const appSettings = {
    databaseURL: 'https://fir-app-1-6e06d-default-rtdb.firebaseio.com/'
 }
 
 const app = initializeApp(appSettings);
+const database = getDatabase(app);
+console.log(database);
+const itemsInDB = ref(database, "items");
+console.log(itemsInDB);
+
 let button = document.getElementById('add-button');
 button.addEventListener("click", addItem);
 
 
 function addItem() {
-   // document.getElementById("demo").innerHTML = "Hello World";
    let curr_item = document.getElementById('input-field').value;
-   console.log(curr_item);
+   console.log(curr_item)
+   push(itemsInDB, curr_item)
  }
