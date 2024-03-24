f1bd66941b785718125abc2e7693db827a00c3c2
Charity <charity.grey1@gmail.com>
2024-03-24 16:09:01 -0700

    imported firebase module and initialized App

--- public/index.js
+++ public/index.js
@@ -1,9 +1,16 @@
-button = document.getElementById('add-button');
+import {initializeApp} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+
+const appSettings = {
+   databaseURL: 'https://fir-app-1-6e06d-default-rtdb.firebaseio.com/'
+}
+
+const app = initializeApp(appSettings);
+let button = document.getElementById('add-button');
 button.addEventListener("click", addItem);
 
 
 function addItem() {
    // document.getElementById("demo").innerHTML = "Hello World";
-   curr_item = document.getElementById('input-field').value;
-   console.log(curr_item)
+   let curr_item = document.getElementById('input-field').value;
+   console.log(curr_item);
  }
