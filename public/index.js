c4fbe178191793b68edf73b81754dffddc7f33d0
Charity <charity.grey1@gmail.com>
2024-03-30 12:16:55 -0700

    Added database reads

--- public/index.js
+++ public/index.js
@@ -1,5 +1,5 @@
 import {initializeApp} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
-import {getDatabase, ref, push} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+import {getDatabase, ref, push, onValue} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
 const appSettings = {
    databaseURL: 'https://fir-app-1-6e06d-default-rtdb.firebaseio.com/'
@@ -7,16 +7,44 @@ const appSettings = {
 
 const app = initializeApp(appSettings);
 const database = getDatabase(app);
-console.log(database);
-const itemsInDB = ref(database, "items");
-console.log(itemsInDB);
+const shoppingListInDB = ref(database, "items");
+console.log(shoppingListInDB);
 
-let button = document.getElementById('add-button');
-button.addEventListener("click", addItem);
+const shoppingListElement = document.getElementById('shopping-list');
+const inputFieldElement = document.getElementById('input-field')
+const submitButtonElement = document.getElementById('add-button');
+submitButtonElement.addEventListener("click", addItem);
 
+onValue(shoppingListInDB, function(snapshot) {
+   clearShoppingListEl();
+   const raw_data = snapshot.val();
+   let data = Object.values(raw_data);
+   for (let item of data) {
+      appendNewShoppingListItem(item);
+   }
+ });
 
 function addItem() {
-   let curr_item = document.getElementById('input-field').value;
-   console.log(curr_item)
-   push(itemsInDB, curr_item)
+   let curr_item = inputFieldElement.value;
+   clearInputFieldEl();
+   if (curr_item != '') {
+      console.log(`${curr_item} pushed to database!`);
+      push(shoppingListInDB, curr_item);
+   }
  }
+
+ function clearInputFieldEl() {
+   inputFieldElement.value = '';
+ }
+
+ function clearShoppingListEl() {
+   shoppingListElement.innerHTML = '';
+ }
+
+ function appendNewShoppingListItem(item) {
+   shoppingListElement.innerHTML = shoppingListElement.innerHTML + `<li>${item}</li>`;
+ 
+ }
+
+ //https://www.youtube.com/watch?v=UFD4SP91tSM Lesson 14 flexbox 1hour
+   
