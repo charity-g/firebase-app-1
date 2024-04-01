af1e44ad8e161af2896eba12ba0a4085b8818980
Charity <charity.grey1@gmail.com>
2024-03-31 17:35:55 -0700

    Added database removal

--- public/index.js
+++ public/index.js
@@ -1,5 +1,5 @@
 import {initializeApp} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
-import {getDatabase, ref, push, onValue} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+import {getDatabase, ref, push, onValue, remove} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
 const appSettings = {
    databaseURL: 'https://fir-app-1-6e06d-default-rtdb.firebaseio.com/'
@@ -17,10 +17,15 @@ submitButtonElement.addEventListener("click", addItem);
 
 onValue(shoppingListInDB, function(snapshot) {
    clearShoppingListEl();
-   const raw_data = snapshot.val();
-   let data = Object.values(raw_data);
-   for (let item of data) {
-      appendNewShoppingListItem(item);
+   const db_snapshot = snapshot;
+   if (db_snapshot.exists()) {
+    let data = Object.entries(db_snapshot.val());
+    for (let item of data) {
+       appendNewShoppingListItem(item);
+    }
+   } else {
+      shoppingListElement.innerHTML = "No items added...";
+      console.log('no snapshot exists');
    }
  });
 
@@ -42,9 +47,16 @@ function addItem() {
  }
 
  function appendNewShoppingListItem(item) {
-   shoppingListElement.innerHTML = shoppingListElement.innerHTML + `<li>${item}</li>`;
- 
+  // shoppingListElement.innerHTML = shoppingListElement.innerHTML + `<li id="${item[0]}">${item[1]}</li>`;
+  let itemID = item[0];
+  let itemValue = item[1];
+  let item_db_location = ref(database, `items/${itemID}`);
+
+  let new_item_el = document.createElement("li");
+  new_item_el.textContent = itemValue;
+  new_item_el.addEventListener("dblclick", function() {remove(item_db_location)});
+  shoppingListElement.append(new_item_el);
  }
 
- //https://www.youtube.com/watch?v=UFD4SP91tSM Lesson 14 flexbox 1hour
+ //https://www.youtube.com/watch?v=UFD4SP91tSM 
    
